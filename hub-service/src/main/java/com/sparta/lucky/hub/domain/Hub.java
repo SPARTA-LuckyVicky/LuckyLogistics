2c4e64117eea6619c4e64bdae7baba8e036bb313
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-31 12:51:15 +0900

    feat: Hub Entity 생성

---
+++ hub-service/src/main/java/com/sparta/lucky/hub/domain/Hub.java
@@ -0,0 +1,39 @@
+package com.sparta.lucky.hub.domain;
+
+import com.sparta.lucky.hub.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_hub", schema = "hub_schema")
+public class Hub extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "manager_id", columnDefinition = "uuid")
+    private UUID managerId;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "address", nullable = false, length = 100)
+    private String address;
+
+    @Column(name = "latitude", nullable = false, precision = 10, scale = 7)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", nullable = false, precision = 10, scale = 7)
+    private BigDecimal longitude;
+}
