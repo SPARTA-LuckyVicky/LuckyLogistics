c0c4e91c2d1ecf60b9613dd30314a8e1fe3ea121
SeongWon Jeong <newon.jsw@gmail.com>
2026-04-01 18:46:42 +0900

    refactor: change error code

--- delivery-service/src/main/java/com/sparta/lucky/deliveryservice/common/response/ResponseCode.java
+++ delivery-service/src/main/java/com/sparta/lucky/deliveryservice/common/response/ResponseCode.java
@@ -4,17 +4,19 @@ import org.springframework.http.HttpStatus;
 
 public enum ResponseCode {
 
-    OK("DELIVERY_200", HttpStatus.OK, "OK"),
-    BAD_REQUEST("DELIVERY_400", HttpStatus.BAD_REQUEST, "Bad Request"),
-    VALIDATION_ERROR("DELIVERY_4001", HttpStatus.BAD_REQUEST, "Validation Error"),
-    NOT_READABLE("DELIVERY_4002", HttpStatus.BAD_REQUEST, "Incorrect request body"),
-    MISSING_HEADER("DELIVERY_4003", HttpStatus.BAD_REQUEST, "Incorrect request header"),
-    FORBIDDEN("DELIVERY_403", HttpStatus.FORBIDDEN, "Access Denied"),
-    NOT_FOUND("DELIVERY_404", HttpStatus.NOT_FOUND, "Resource Not Found"),
-    METHOD_NOT_ALLOWED("DELIVERY_405", HttpStatus.METHOD_NOT_ALLOWED, "Method Not Allowed"),
-    INTERNAL_ERROR("DELIVERY_500", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+    OK("DELIVERY_000", HttpStatus.OK, "OK"),
+    BAD_REQUEST("DELIVERY_001", HttpStatus.BAD_REQUEST, "Bad Request"),
+    VALIDATION_ERROR("DELIVERY_002", HttpStatus.BAD_REQUEST, "Validation Error"),
+    NOT_READABLE("DELIVERY_003", HttpStatus.BAD_REQUEST, "Incorrect request body"),
+    MISSING_HEADER("DELIVERY_004", HttpStatus.BAD_REQUEST, "Incorrect request header"),
+    FORBIDDEN("DELIVERY_005", HttpStatus.FORBIDDEN, "Access Denied"),
+    NOT_FOUND("DELIVERY_006", HttpStatus.NOT_FOUND, "Resource Not Found"),
+    METHOD_NOT_ALLOWED("DELIVERY_007", HttpStatus.METHOD_NOT_ALLOWED, "Method Not Allowed"),
+    INTERNAL_ERROR("DELIVERY_008", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
 
-    DRIVER_EXISTS("DELIVERY_4093", HttpStatus.CONFLICT, "Driver already exists"),
+    // DRIVER(3)
+    DRIVER_CREATED("DELIVERY_300", HttpStatus.CREATED, "Driver Created"),
+    DRIVER_EXISTS("DELIVERY_301", HttpStatus.CONFLICT, "Driver already exists"),
     ;
 
     private final String code;
