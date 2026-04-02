804e8a849a359ed4f700a14f49419861d3b6641b
subin <143770193+LSLight@users.noreply.github.com>
2026-04-02 11:10:03 +0900

    [Feat] notification-service 기본 세팅  (#50)
    * chore: notification-service 기본 세팅
    * feat: common 공통 모듈 생성
    * feat: ai/slack entity/repository 생성, 에러코드 기본 추가
    * fix: 코드래빗 권장 수정사항으로 개선

---
+++ notification-service/src/main/java/com/sparta/lucky/notification/domain/AiMessageRepository.java
@@ -0,0 +1,16 @@
+package com.sparta.lucky.notification.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AiMessageRepository  extends JpaRepository<AiMessage, UUID> {
+
+    Page<AiMessage> findByRelatedOrderId(UUID relatedOrderId, Pageable pageable);
+
+    Optional<AiMessage> findBySlackMessageId(UUID slackMessageId);
+
+}
---
+++ notification-service/src/main/java/com/sparta/lucky/notification/domain/SlackMessage.java
@@ -0,0 +1,73 @@
+package com.sparta.lucky.notification.domain;
+
+
+import com.sparta.lucky.notification.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_slack_message")
+@SQLRestriction("deleted_at IS NULL")
+public class SlackMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String receiverSlackId;
+
+    @Column(nullable = false)
+    private String messageContent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MessageType messageType;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt;
+
+    // 관련 주문 ID (주문 알림일 경우)
+    @Column(columnDefinition = "uuid")
+    private UUID relatedOrderId;
+
+    // 발송자 (내부 시스템은 null 가능)
+    @Column(columnDefinition = "uuid")
+    private UUID senderId;
+
+    public static SlackMessage create(
+            String receiverSlackId,
+            String messageContent,
+            MessageType messageType,
+            UUID relatedOrderId,
+            UUID senderId
+    ){
+        SlackMessage msg = new SlackMessage();
+        msg.receiverSlackId = receiverSlackId;
+        msg.messageContent = messageContent;
+        msg.messageType = messageType;
+        msg.sentAt = LocalDateTime.now();
+        msg.relatedOrderId = relatedOrderId;
+        msg.senderId = senderId;
+        return msg;
+    }
+
+    public void softDelete(UUID deletedBy) {
+        super.softDelete(deletedBy);
+    }
+
+
+
+
+}
