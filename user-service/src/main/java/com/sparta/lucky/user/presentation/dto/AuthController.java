f6a987a341cabaf491a76dbeff80c4288f94a9b4
kim-jinyeong <rlawlsdud51@naver.com>
2026-04-03 06:29:09 +0900

    feat: User AuthController 구현

---
+++ user-service/src/main/java/com/sparta/lucky/user/presentation/dto/AuthController.java
@@ -0,0 +1,25 @@
+package com.sparta.lucky.user.presentation.dto;
+
+import com.sparta.lucky.user.application.AuthService;
+import com.sparta.lucky.user.application.dto.response.SignupResult;
+import com.sparta.lucky.user.common.response.ApiResponse;
+import com.sparta.lucky.user.presentation.dto.request.SignupReqDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ApiResponse<SignupResult> signup(@Valid @RequestBody SignupReqDto reqDto){
+        SignupResult signupResult = authService.signup(reqDto);
+        return ApiResponse.success(signupResult);
+    }
+}
