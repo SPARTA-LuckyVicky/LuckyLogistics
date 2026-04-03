ff986996c976b10e2ff85f9ba726209d9f8d6759
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-04-03 12:31:51 +0900

    feat: Gateway 헤더 기반 인증 필터 및 역할 권한 처리 적용 (현재는 모든 요청 permitAll 임시)

--- hub-service/src/main/java/com/sparta/lucky/hub/common/config/SecurityConfig.java
+++ hub-service/src/main/java/com/sparta/lucky/hub/common/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.sparta.lucky.hub.common.config;
 
+import com.sparta.lucky.hub.common.filter.HeaderAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -25,8 +28,8 @@ public class SecurityConfig {
 //                        .anyRequest().authenticated()
                         // 현재는 모든 요청 permitAll
                         .anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+                );
+//                .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
