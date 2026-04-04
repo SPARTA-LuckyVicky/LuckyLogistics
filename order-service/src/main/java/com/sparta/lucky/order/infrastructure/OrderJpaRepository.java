50b117fa44e51b550b4de006f7b5df70ec050596
Lee subin <99sbin@gmail.com>
2026-04-04 19:21:03 +0900

    fix: JpaRepository OR/AND 우선순위 문제 -> @Query 추가

--- order-service/src/main/java/com/sparta/lucky/order/infrastructure/OrderJpaRepository.java
+++ order-service/src/main/java/com/sparta/lucky/order/infrastructure/OrderJpaRepository.java
@@ -5,28 +5,44 @@ import com.sparta.lucky.order.domain.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface OrderJpaRepository extends JpaRepository<Order, UUID> {
+
     Page<Order> findByStatus(OrderStatus status, Pageable pageable);
+
     // 업체 담당자용
+    @Query("""
+            SELECT o FROM Order o
+            WHERE (o.requesterCompanyId = :requesterCompanyId OR o.receiverCompanyId = :receiverCompanyId)
+            AND o.status = :status
+            """)
     Page<Order> findByRequesterCompanyIdOrReceiverCompanyIdAndStatus(
-            UUID requesterCompanyId, UUID receiverCompanyId,
-            OrderStatus status, Pageable pageable);
+            @Param("requesterCompanyId") UUID requesterCompanyId,
+            @Param("receiverCompanyId") UUID receiverCompanyId,
+            @Param("status") OrderStatus status,
+            Pageable pageable);
 
     Page<Order> findByRequesterCompanyIdOrReceiverCompanyId(
             UUID requesterCompanyId, UUID receiverCompanyId,
             Pageable pageable);
 
     // 허브 매니저용
+    @Query("""
+            SELECT o FROM Order o
+            WHERE (o.originHubId = :originHubId OR o.destinationHubId = :destinationHubId)
+            AND o.status = :status
+            """)
     Page<Order> findByOriginHubIdOrDestinationHubIdAndStatus(
-            UUID originHubId, UUID destinationHubId,
-            OrderStatus status, Pageable pageable);
+            @Param("originHubId") UUID originHubId,
+            @Param("destinationHubId") UUID destinationHubId,
+            @Param("status") OrderStatus status,
+            Pageable pageable);
 
     Page<Order> findByOriginHubIdOrDestinationHubId(
             UUID originHubId, UUID destinationHubId,
             Pageable pageable);
-
-
 }
