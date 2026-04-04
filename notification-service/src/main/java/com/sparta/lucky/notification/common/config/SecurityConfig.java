d47e6f61c8f5cba658e7c0e98e26ca525675ee0a
Lee subin <99sbin@gmail.com>
2026-04-04 18:27:21 +0900

    feat: config 수정 - 구현한 filter SecurityConfig 에 추가

--- notification-service/src/main/java/com/sparta/lucky/notification/common/config/SecurityConfig.java
+++ notification-service/src/main/java/com/sparta/lucky/notification/common/config/SecurityConfig.java
@@ -1,25 +1,39 @@
 package com.sparta.lucky.notification.common.config;
 
+import com.sparta.lucky.notification.common.filter.HeaderAuthenticationFilter;
+import com.sparta.lucky.notification.common.filter.InternalRequestFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final HeaderAuthenticationFilter headerAuthenticationFilter;
+    private final InternalRequestFilter internalRequestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/internal/**").permitAll() // 내부 API는 인증 제외
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger
+                        .requestMatchers("/internal/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(headerAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(internalRequestFilter,
+                        HeaderAuthenticationFilter.class)
                 .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
         return http.build();
     }
