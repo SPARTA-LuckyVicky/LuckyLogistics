ecd736bf4b08d5abf4f96ecf1734ea27ecf6a8b5
Lee subin <99sbin@gmail.com>
2026-04-04 18:28:36 +0900

    fix: filter @NonNull 어노테이션 추가

--- notification-service/src/main/java/com/sparta/lucky/notification/common/filter/InternalRequestFilter.java
+++ notification-service/src/main/java/com/sparta/lucky/notification/common/filter/InternalRequestFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,9 +19,9 @@ public class InternalRequestFilter extends OncePerRequestFilter {
     private static final String INTERNAL_HEADER = "X-Internal-Request";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         if (request.getRequestURI().startsWith(INTERNAL_PATH_PREFIX)) {
             validateInternalRequest(request.getHeader(INTERNAL_HEADER));
         }
