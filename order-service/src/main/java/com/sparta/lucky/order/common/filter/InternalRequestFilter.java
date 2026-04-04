446d11e8b416b2383eee91b78c5c070ca7b1395c
Lee subin <99sbin@gmail.com>
2026-04-04 18:00:43 +0900

    refactor: @NonNull 어노테이션 추가로 필터 경고 제거

--- order-service/src/main/java/com/sparta/lucky/order/common/filter/InternalRequestFilter.java
+++ order-service/src/main/java/com/sparta/lucky/order/common/filter/InternalRequestFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -18,9 +19,9 @@ public class InternalRequestFilter extends OncePerRequestFilter {
     private static final String INTERNAL_HEADER = "X-Internal-Request";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         if (request.getRequestURI().startsWith(INTERNAL_PATH_PREFIX)) {
             validateInternalRequest(request.getHeader(INTERNAL_HEADER));
         }
