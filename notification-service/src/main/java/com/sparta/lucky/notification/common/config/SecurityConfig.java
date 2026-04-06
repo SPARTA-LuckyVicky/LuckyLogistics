ec97da538513b9692c46d884e31a34b640cc66e7
Lee subin <99sbin@gmail.com>
2026-04-06 13:01:17 +0900

    fix: 사용하지않는 import 제거

--- notification-service/src/main/java/com/sparta/lucky/notification/common/config/SecurityConfig.java
+++ notification-service/src/main/java/com/sparta/lucky/notification/common/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.sparta.lucky.notification.common.filter.InternalRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
