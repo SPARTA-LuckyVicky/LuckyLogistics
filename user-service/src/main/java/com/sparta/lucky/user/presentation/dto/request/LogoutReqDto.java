5933f37816f7021d049d6b925073ee46e1a7d71d
kim-jinyeong <rlawlsdud51@naver.com>
2026-04-06 18:06:08 +0900

    feat: auth 관련 기능 구현

---
+++ user-service/src/main/java/com/sparta/lucky/user/presentation/dto/request/LogoutReqDto.java
@@ -0,0 +1,12 @@
+package com.sparta.lucky.user.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutReqDto {
+    private String refreshToken;
+}
