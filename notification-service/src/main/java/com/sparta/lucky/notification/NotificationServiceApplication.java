7709b69cc0e133bb6b59145b6f8ce8464cb24fde
Lee subin <99sbin@gmail.com>
2026-04-06 11:06:49 +0900

    chore(order-noti): Zipkin 트레이싱 추가 설정

--- notification-service/src/main/java/com/sparta/lucky/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/sparta/lucky/notification/NotificationServiceApplication.java
@@ -1,10 +1,11 @@
 package com.sparta.lucky.notification;
 
+import com.sparta.lucky.notification.infrastructure.client.FeignConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignConfig.class)
 @SpringBootApplication
 public class NotificationServiceApplication {
 
