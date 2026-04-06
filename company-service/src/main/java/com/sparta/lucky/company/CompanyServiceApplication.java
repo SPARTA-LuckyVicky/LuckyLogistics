495ae7578bea9d6f5fc377e1deb6d5e6a77db523
Dong Jin Park <160587239+straycat405@users.noreply.github.com>
2026-04-06 11:12:46 +0900

    chore: zipkin 트레이스 추적 설정 추가 (#89)

--- company-service/src/main/java/com/sparta/lucky/company/CompanyServiceApplication.java
+++ company-service/src/main/java/com/sparta/lucky/company/CompanyServiceApplication.java
@@ -1,11 +1,12 @@
 package com.sparta.lucky.company;
 
+import com.sparta.lucky.company.infrastructure.feign.FeignConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignConfig.class)
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
